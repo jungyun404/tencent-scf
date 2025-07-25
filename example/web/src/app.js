e65fec64f6bee74f8981109a60a4c20296ec2e98
JungYunji <jungyunji404@gmail.com>
2025-07-25 16:40:48 +0900

    [Autofic] 3 malicious code detected!!

--- example/web/src/app.js
+++ example/web/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.disable('x-powered-by') // Disable the X-Powered-By header
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
